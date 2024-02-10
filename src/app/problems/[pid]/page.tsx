224237444a7cefdbbbf54b153c6469c99ef95014
Harsh <harshturna15@gmail.com>
2024-02-09 21:26:31 -0600

    updates

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -1,13 +1,34 @@
 import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
 import { problems } from "@/constants/code-problems";
+import { firestore } from "@/firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
-const ProblemsPage = ({ params }: { params: { pid: string } }) => {
+async function getProblemData(problemId: string) {
+  try {
+    const docRef = doc(firestore, "problems", problemId);
+    const problemData = await getDoc(docRef);
+
+    if (!problemData) return null;
+
+    return problemData;
+  } catch (error) {
+    return null;
+  }
+}
+
+const ProblemsPage = async ({ params }: { params: { pid: string } }) => {
   if (!params.pid || !problems[params.pid]) {
     return <div>Problem not found</div>;
   }
-
   const problem = problems[params.pid];
+
+  // console.log(params.pid);
+
+  const problemData = await getProblemData(params.pid);
+
+  console.log(problemData);
+
   problem.handlerFunction = problem.handlerFunction.toString();
   return (
     <div>
