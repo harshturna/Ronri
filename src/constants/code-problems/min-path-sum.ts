52b4c8240cdc89c8966fa80cdf35de28cd322d5f
Harsh <74731135+harshturna@users.noreply.github.com>
2024-02-26 21:43:59 -0600

    Update min-path-sum.ts

--- src/constants/code-problems/min-path-sum.ts
+++ src/constants/code-problems/min-path-sum.ts
@@ -45,7 +45,7 @@ function minPathSum(grid) {
 
 export const minPathSum: Problem = {
   id: "min-path-sum",
-  title: "Minimum Path Sum",
+  title: "9. Minimum Path Sum",
   problemStatement: `
         <p class='mt-3'>
         Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path. Note: You can only move either down or right at any point in time.
